2c96d2e52be9c765b1fe93ae5a7d920da5ded9b2
Denis Yakovlev <de1m0s242@gmail.com>
2020-03-10 02:27:01 +0700

    #1 add jwt token authorization

--- family_api/main.py
+++ family_api/main.py
@@ -2,6 +2,7 @@ import asyncio
 
 from aiohttp import web
 from aiohttp_apispec import setup_aiohttp_apispec
+from aiohttp_jwt import JWTMiddleware
 from aiopg.sa import create_engine
 
 from family_api.config import Config
@@ -12,10 +13,24 @@ async def create_db_connection():
     return await create_engine(Config.DATABASE_URI)
 
 
+def create_jwt_middleware():
+    return JWTMiddleware(secret_or_pub_key=Config.JWT_PUBLIC_KEY,
+                         request_property='token_data',
+                         credentials_required=True,
+                         whitelist=['/swagger-ui', '/static*', '/api/docs*'])
+
+
 async def init_app():
-    app = web.Application()
+    jwt_middleware = create_jwt_middleware()
+    app = web.Application(middlewares=[jwt_middleware])
     setup_routes(app)
-    setup_aiohttp_apispec(app=app, title="Family service", version="v1", swagger_path='/swagger-ui')
+    setup_aiohttp_apispec(app=app, title="Family service", version="v1", swagger_path='/swagger-ui',
+                          securityDefinitions={"jwt": {"type": "apiKey",
+                                                       "schema": "bearer",
+                                                       "bearerFormat": "JWT",
+                                                       "name": "Authorization",
+                                                       "in": "header"}},
+                          security=[{"jwt": []}])
     app['db'] = await create_db_connection()
     return app
 
--- family_api/views/family_view.py
+++ family_api/views/family_view.py
@@ -1,8 +1,11 @@
+from uuid import UUID
+
 from aiohttp import web
 from aiohttp_apispec import docs, request_schema, response_schema
 from marshmallow import Schema, fields
 
 from family_api.controllers.family import register_family
+from family_api.controllers.family_member import add_family_member
 
 
 class FamilySchema(Schema):
@@ -20,4 +23,5 @@ class FamilyListView(web.View):
         family_obj = schema.load(await self.request.json())
         async with self.request.app['db'].acquire() as conn:
             stored_family = await register_family(family_obj, conn)
+            await add_family_member(stored_family, UUID(self.request['token_data']['user_claims']['uuid']), conn)
             return web.json_response(schema.dump(stored_family))
